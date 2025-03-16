7cca3d45c74ec519c4ae24dc93cb25717e296627
NABIL <nabilkechiche27@gmail.com>
2025-03-17 00:52:31 +0100

    UPD

--- java.js
+++ java.js
@@ -1,10 +1,30 @@
-let icon = document.querySelector('.icon');
-let nav = document.querySelector('.navbar');
-let bars = document.querySelector('.fa-bars');
-let xmark = document.querySelector('.fa-xmark');
+//span-js
+let span1 = document.querySelector(".span-js");
+let text = ["Developer","Web"];
+let i = 0;
 
-icon.addEventListener('click' , () => {
-    bars.classList.toggle('active');
-    xmark.classList.toggle('active');
-    nav.classList.toggle('active')
-})
+function spanText() {
+  span1.innerHTML = text[i];
+  i++;
+
+  if (i >= text.length) {
+    i = 0;
+  }
+
+  setTimeout(function () {
+    spanText();
+  }, 1500);
+}
+spanText();
+
+// nav bar
+let icon = document.querySelector(".icon");
+let nav = document.querySelector(".navbar");
+let bars = document.querySelector(".fa-bars");
+let xmark = document.querySelector(".fa-xmark");
+
+icon.addEventListener("click", () => {
+  bars.classList.toggle("active");
+  xmark.classList.toggle("active");
+  nav.classList.toggle("active");
+});
